0579e0cb69b0dfe4d92044c9b8aafbcdc565e91b
Fabian Sorqvist <fabian@sorqvist.nu>
2017-10-22 17:45:10 +0200

    cleaner browsers script

--- parts/browsers.sh
+++ parts/browsers.sh
@@ -1,10 +1,7 @@
 #! /bin/bash
 
 sudo add-apt-repository -y ppa:mozillateam/firefox-next
-sudo apt-get update
-sudo apt-get install firefox
-
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+sudo apt-get install -y google-chrome-stable firefox
