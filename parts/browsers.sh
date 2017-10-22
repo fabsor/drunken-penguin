894ba359fed6810fd4e42c9685b9f27f66de1c28
Fabian Sorqvist <fabian@sorqvist.nu>
2017-10-22 17:43:03 +0200

    more fixes

--- parts/browsers.sh
+++ parts/browsers.sh
@@ -4,7 +4,7 @@ sudo add-apt-repository -y ppa:mozillateam/firefox-next
 sudo apt-get update
 sudo apt-get install firefox
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key -y add -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
 sudo apt-get install -y google-chrome
--- parts/essentials.sh
+++ parts/essentials.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 sudo apt-get update
 sudo apt-get install curl git
-sudo add-apt-repository ppa:ubuntu-desktop/ubuntu-make
