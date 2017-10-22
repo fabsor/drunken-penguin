31773ea1bf9f75f6602e02edaa0ce52fdcc6cdab
Fabian Sorqvist <fabian@sorqvist.nu>
2017-10-22 17:40:22 +0200

    more browsers

--- parts/browsers.sh
+++ parts/browsers.sh
@@ -3,3 +3,8 @@
 sudo add-apt-repository -y ppa:mozillateam/firefox-next
 sudo apt-get update
 sudo apt-get install firefox
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key -y add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get update
+sudo apt-get install -y google-chrome
