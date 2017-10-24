82942b9f7d56af2fbec22b8c5303a8556b61864b
Fabian Sorqvist <fabian@sorqvist.nu>
2017-10-24 13:05:44 +0200

    update vagrant and virtualbox

--- parts/vagrant.sh
+++ parts/vagrant.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
 if [ ! -f /usr/bin/vagrant ]; then
-    wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/1.9.7/vagrant_1.9.7_x86_64.deb
+    wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.deb
     sudo dpkg -i /tmp/vagrant.deb
 fi
 
 if [ ! -f /usr/bin/virtualbox ]; then
-    wget -O /tmp/virtualbox.deb http://download.virtualbox.org/virtualbox/5.1.26/virtualbox-5.1_5.1.26-117224~Ubuntu~zesty_amd64.deb
+    wget -O /tmp/virtualbox.deb http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~zesty_amd64.deb
     sudo dpkg -i /tmp/virtualbox.deb
 fi
