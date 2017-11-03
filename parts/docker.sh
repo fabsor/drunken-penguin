8246330e31d14c50cbe324b1afbc07d6dc8d69c3
Fabian Sorqvist <fabian@sorqvist.nu>
2017-11-03 18:22:04 +0100

    more docker fixes

--- parts/docker.sh
+++ parts/docker.sh
@@ -1,20 +1,11 @@
 #! /bin/bash
 
-apt-get install -y \
-     apt-transport-https \
-     ca-certificates \
-     curl \
-     software-properties-common
-
+apt-get update
+apt-get install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-
 apt-key fingerprint 0EBFCD88
 
-add-apt-repository \
-     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
 
 apt-get update
-
-apt-get install -y docker-ce
+apt-get install docker-ce
