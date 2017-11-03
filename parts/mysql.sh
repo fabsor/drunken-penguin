549e11d0217bf330782ad3ca94019a2f42175f21
Fabian Sorqvist <fabian@sorqvist.nu>
2017-11-03 19:50:10 +0100

    this should do it

--- parts/mysql.sh
+++ parts/mysql.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 sudo apt-get update
-sudo export DEBIAN_FRONTEND=noninteractive
-sudo apt-get install -y mysql-server mysql-common libmysqlclient-dev
+sudo export DEBIAN_FRONTEND=noninteractive && apt-get install -y mysql-server mysql-common libmysqlclient-dev
 # A user that can be used for all conections.
 echo "GRANT ALL PRIVILEGES ON *.* TO 'admin'@'localhost' identified by '' WITH GRANT OPTION" | sudo mysql
