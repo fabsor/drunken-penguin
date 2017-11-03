4f6e597380d2be5210d01719644c266f74689ec1
Fabian Sorqvist <fabian@sorqvist.nu>
2017-11-03 15:57:09 +0100

    vagrant fix

--- parts/vagrant.sh
+++ parts/vagrant.sh
@@ -3,10 +3,10 @@ set -e
 apt-get install -y gdebi
 if [ ! -f /usr/bin/vagrant ]; then
     wget -O /tmp/vagrant.deb https://releases.hashicorp.com/vagrant/2.0.0/vagrant_2.0.0_x86_64.deb
-    gdebi -y /tmp/vagrant.deb
+    gdebi -n /tmp/vagrant.deb
 fi
 
 if [ ! -f /usr/bin/virtualbox ]; then
     wget -O /tmp/virtualbox.deb http://download.virtualbox.org/virtualbox/5.2.0/virtualbox-5.2_5.2.0-118431~Ubuntu~zesty_amd64.deb
-    gdebi -y /tmp/virtualbox.deb
+    gdebi -n /tmp/virtualbox.deb
 fi
