1104281f11e10dd4393afa35ccdf8fd2f35ea808
Fabian Sorqvist <fabian@sorqvist.nu>
2018-09-22 16:00:10 +0200

    remove shell stuff

--- parts/shell.sh
+++ parts/shell.sh
@@ -3,9 +3,5 @@
 # Install zsh
 sudo apt-get install -y zsh
 
-# Oh my zsh is what you have been dreaming about
-# in the shower while awkwardly touching yourself.
-# Let's make that secret dream a reality.
-if [ ! -d ~/.oh-my-zsh ]; then
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+echo "Install oh-my-sh:"
+echo "curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
