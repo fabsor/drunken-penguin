596d32991c86417dd38b0ebcfb33ee8b78bd878b
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-13 21:27:55 +0200

    Docker

--- parts/docker.sh
+++ parts/docker.sh
@@ -1,13 +1,4 @@
 #! /bin/bash
 
-source /etc/os-release
-
-sudo apt-get update
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $VERSION_CODENAME stable"
-
-sudo apt-get update
-sudo apt-get install -y docker-ce
+sudo pacman -S --noconfirm docker
+sudo systemctl enable docker.service
