9e576e80e2b2b8ad4e1c87f5e06d46674fd595c2
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-13 21:53:49 +0200

    remove drupal

--- parts/drupal.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-sudo apt-get install php-cli
-if [ ! -f /usr/local/bin/drush ]; then
-    sudo wget -O /usr/local/bin/drush https://github.com/drush-ops/drush-launcher/releases/download/0.4.2/drush.phar
-    sudo chmod +x /usr/local/bin/drush
-fi
