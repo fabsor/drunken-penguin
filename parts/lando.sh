f1d3b3d926d7178f0592a03b8e95df2c91888f01
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-13 21:28:09 +0200

    Lando

--- parts/lando.sh
+++ parts/lando.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 set -e
 if [ ! -f /usr/local/bin/lando ]; then
-    wget -O /tmp/lando.deb https://github.com/lando/lando/releases/download/v3.0.0-rc.1/lando-v3.0.0-rc.1.deb
-    sudo dpkg -i /tmp/lando.deb
+    wget -O /tmp/lando.pacman https://github.com/lando/lando/releases/download/v3.0.0-rc.1/lando-v3.0.0-rc.1.pacman
+    sudo pacman -U /tmp/lando.pacman
 fi
