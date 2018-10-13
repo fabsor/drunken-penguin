8cd34ae80e11c7bc9ae586d03212679e07651f45
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-13 20:33:59 +0200

    fix shell

--- parts/shell.sh
+++ parts/shell.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Install zsh
-sudo apt-get install -y zsh
+sudo pacman -S --noconfirm zsh
 
 echo "Install oh-my-sh:"
 echo "curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh"
