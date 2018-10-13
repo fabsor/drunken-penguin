cb878a4c21689c680160393ab0228661754630af
Fabian Sorqvist <fabian@sorqvist.nu>
2018-10-13 21:28:15 +0200

    Leisure stuff

--- parts/leisure.sh
+++ parts/leisure.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
+git clone https://aur.archlinux.org/spotify-stable.git /tmp/spotify-stable
+pushd /tmp/spotify-stable
+makepkg --skippgpcheck -si
+popd
 
-sudo snap install -y spotify
-sudo snap install -y signal
+git clone https://aur.archlinux.org/slack-desktop.git /tmp/slack
+pushd /tmp/slack
+makepkg -si
+popd
+
+git clone https://aur.archlinux.org/nvm.git /tmp/nvm
+pushd /tmp/nvm
+makepkg -si
+popd
+
+git clone https://aur.archlinux.org/signal.git /tmp/signal
+pushd /tmp/signal
+makepkg -si
+popd
 
